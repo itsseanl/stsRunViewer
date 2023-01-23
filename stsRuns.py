4da5edc3cddb0dfa07aa1f3325e7cfb686e3f1ad
Sean Lyons <itsseanl@gmail.com>
2023-01-23 02:48:05 -0500

    added relics.json, updated return_wins method, added new methods to pull card and relic descriptions

--- stsRuns.py
+++ stsRuns.py
@@ -25,13 +25,42 @@ class stsRuns:
 
     # returns wins
     def return_wins(self, deck=None, relics=None, allData=None):
-        if (deck):
             for win in self.wins:
-                print("winning deck: ")
-                print(win["master_deck"])
-        if (relics):
-            for win in self.wins:
-                print("winning relic: ")
-                print(win["relics"])
-        if (allData):
-            print(self.wins)
+                if (deck):
+                    print("winning deck: ")
+                    self.return_deck(win["master_deck"])
+                if (relics):
+                        print("winning relics: ")
+                        self.return_relics(win["relics"])
+                if (allData):
+                    print(self.wins)
+
+    def return_deck(self, deck):
+        deck_descriptions = []
+        with open("./cards.json", "r") as cards_list:
+            file_contents = cards_list.read()
+            master_cards = json.loads(file_contents)
+            # print(master_cards["A Thousand Cuts"])
+            # print(type(master_cards))
+        for card in deck:
+            for k, v in master_cards.items():
+               
+                if (card == k):
+                    deck_descriptions.append({'name': k, 'description': v["DESCRIPTION"] })
+        print(deck_descriptions)
+    
+    def return_relics(self, relics):
+        relics_descriptions = []
+        with open("./relics.json", "r") as relic_list:
+            file_contents = relic_list.read()
+            master_relics = json.loads(file_contents)
+            # print(master_cards["A Thousand Cuts"])
+            # print(type(master_cards))
+        for relic in relics:
+            for k, v in master_relics.items():
+                if (relic == k):
+                    # print(k)
+                    # print(v["DESCRIPTION"])
+                    s = " "
+                    relics_descriptions.append({'name': k, 'description': s.join(v["DESCRIPTIONS"]) })
+        print(relics_descriptions)
